fc8192269c4e595c32ef96e03e6fb5b72287b79b
mujammil maldar <mujammilmaldar65@gmail.com>
2025-02-23 15:28:56 +0200

    last update without mobile responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -373,3 +373,25 @@ $(document).ready(function() {
 //     });
 // });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const hamburger = document.getElementById("hamburger");
+const navItems = document.getElementById("nav-items");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navItems.classList.toggle("active");
+});
